97f4f1a34d9d3d45566c9e84e884c1cece48b87e
JulioMS2003 <julimanchay4862@gmail.com>
2024-09-23 18:35:35 -0500

    init proyect

---
+++ src/main/java/pe/edu/cibertec/app_examen/AppExamenApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExamenApplication.class, args);
+	}
+
+}
